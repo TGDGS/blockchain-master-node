127d6f37689d9d4d430340314ea3e808ba8eebd7
TGDGS <111734730+TGDGS@users.noreply.github.com>
2023-11-08 11:10:37 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,16 @@
 radio.setGroup(90)
+radio.setTransmitPower(7)
+radio.setFrequencyBand(5)
+let Block_Reward = 1
+let Block_No = 0
+let Total_Blocks = 0
+radio.sendValue("Block_Reward", Block_Reward)
+radio.sendValue("Block_No", Block_No)
+radio.sendValue("Total_Blocks", Total_Blocks)
+serial.redirectToUSB()
+serial.writeLine("Block Reward Is:" + Block_Reward)
+serial.writeLine("Total Blocks Is:" + Total_Blocks)
+serial.writeLine("No. Of Blocks:" + Block_No)
 basic.forever(function () {
 	
 })
