9b042fa01436212ac4f0f63bfb0ecf7b41baddb1
TGDGS <111734730+TGDGS@users.noreply.github.com>
2023-11-08 14:48:00 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,22 +2,29 @@ enum RadioMessage {
     message1 = 49434,
     Mining_Started = 16113
 }
-radio.onReceivedValue(function (name, value) {
-    while (name == "Mining_Started" && value == 1) {
-        basic.showString("ID 1 Mining")
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
         serial.writeString("ID 1 Has Started Mining")
-        Total_Blocks += Total_Blocks + 1
-        Block_No += Block_No + 1
+        Total_Blocks += 0 + 1
+        Block_No += 0 + 1
+        basic.pause(1000)
+    } else if (receivedNumber == 0) {
+        ID_1 = 0
     }
 })
-let Total_Blocks = 0
-let Block_No = 0
+radio.onReceivedMessage(RadioMessage.message1, function () {
+    led.toggle(0, 0)
+    serial.writeLine("ID 1 Is Online")
+    basic.pause(5000)
+    basic.clearScreen()
+})
+let ID_1 = 0
 radio.setGroup(90)
 radio.setTransmitPower(7)
 radio.setFrequencyBand(5)
 let Block_Reward = 1
-Block_No = 0
-Total_Blocks = 0
+let Block_No = 0
+let Total_Blocks = 0
 radio.sendValue("Block_Reward", Block_Reward)
 radio.sendValue("Block_No", Block_No)
 radio.sendValue("Total_Blocks", Total_Blocks)
@@ -25,8 +32,12 @@ serial.redirectToUSB()
 serial.writeLine("Block Reward Is:" + Block_Reward)
 serial.writeLine("Total Blocks Is:" + Total_Blocks)
 serial.writeLine("No. Of Blocks:" + Block_No)
-loops.everyInterval(100, function () {
-    radio.sendValue("Block_Reward", Block_Reward)
-    radio.sendValue("Block_No", Block_No)
-    radio.sendValue("Total_Blocks", Total_Blocks)
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    basic.pause(1000)
+    serial.writeLine("Block Reward Is:" + Block_Reward)
+    serial.writeLine("Total Blocks Is:" + Total_Blocks)
+    serial.writeLine("No. Of Blocks:" + Block_No)
 })
